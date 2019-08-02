90ee9aaa70ee5d53baa1699eab76f7b0b4ead38a
Katerina Plushko <katplush@yahoo.com>
2019-08-01 18:38:01 -0700

    add router.js, moved HelloWorld homepage from components to views

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
+
+
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App), 
+  router
 }).$mount('#app')
+
